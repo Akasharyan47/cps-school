b4363d21433c32891b49353287deb7cae9d4fabf
Akasharyan47 <103964781+Akasharyan47@users.noreply.github.com>
2022-08-18 20:55:18 +0530

    Update teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -1,61 +1,8 @@
 //  teacher list show
+ 
+teachersdata(); 
 
-
-// function fetchData() {
-//     fetch("https://yg1y0egp9c.execute-api.ap-south-1.amazonaws.com/dev/teachers-detail")
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw Error("ERROR");
-//             }
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log(data.data);
-//             const html = data.data
-//                 .map(user => {
-//                     return `
-//                     <div class="column">
-//                         <div class="column1">
-//                             <div class="a">
-//                                 <p><img src="${user.avatar}" alt="${user.first_name}" ></p>
-//                             </div>
-                            
-//                             <div class="b">
-//                                  <p>Name: ${user.first_name}</p>
-//                                 <p>Email: ${user.email}</p> 
-//                             </div> 
-                            
-//                         </div> 
-//                     </div>
-//                     <div class="column">
-//                     <div class="column1">
-//                         <div class="a">
-//                             <p><img src="${user.avatar}" alt="${user.first_name}" ></p>
-//                         </div>
-                        
-//                         <div class="b">
-//                              <p>Name: ${user.first_name}</p>
-//                             <p>Email: ${user.email}</p> 
-//                         </div> 
-                        
-//                     </div> 
-//                 </div>
-          
-                    
-//                     `;
-//                 })
-//                 .join("");
-//             console.log(html)
-//             document.querySelector('#teacherlist').insertAdjacentHTML('afterbegin', html);
-
-//         }).catch(error => {
-//             console.log(error);
-//         })
-
-// }
-fetchData(); 
-
-function fetchData(){
+function teachersdata(){
     fetch('https://yg1y0egp9c.execute-api.ap-south-1.amazonaws.com/dev/teachers-detail',{
     method:'GET', 
     headers:{
