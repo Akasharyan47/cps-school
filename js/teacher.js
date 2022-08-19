c0572f0ee2660eb12a9ef3fd937889a61c5ec2d3
Akasharyan47 <103964781+Akasharyan47@users.noreply.github.com>
2022-08-20 00:36:00 +0530

    Update teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -1,41 +1,42 @@
 //  teacher list show
- 
-teachersdata(); 
 
-function teachersdata(){
-    fetch('https://yg1y0egp9c.execute-api.ap-south-1.amazonaws.com/dev/teachers-detail',{
-    method:'GET', 
-    headers:{
-      'content-type': 'application/json',
-      'Accept': 'application/json',
-    }, 
-    body: JSON.stringify(),
-  })
-  
-    .then((response) => response.json()) 
-    .then((response) => { 
-      if (response.apiStatus == true) {  
-          console.log(response.teachersDetail); 
-                      const html = response.teachersDetail
-                .map(user => {
-                    return `
-                    <div class="column">
-                        <div class="column1">
-                            <div class="a">
+teachersdata();
+
+function teachersdata() {
+    fetch('https://yg1y0egp9c.execute-api.ap-south-1.amazonaws.com/dev/teachers-detail', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(),
+    })
+
+        .then((response) => response.json())
+        .then((response) => {
+            if (response.apiStatus == true) {
+                console.log(response.teachersDetail);
+                const html = response.teachersDetail
+                    .map(user => {
+                        return `
+                    <div class="teachercolumn">
+                        <div class="teachercolumn1">
+                            <div class="a  ">
                                 <p><img src="${user.imageUrl}" alt="${user.name}" ></p>
                             </div>
                             
-                            <div class="b">
-                                 <p><b>Name:</b> ${user.name}</p>
-                                 <p><b>Subject:</b> ${user.subjects}</p> 
+                            <div class="b row"> 
+                                <a><p>Name:</p>${user.name}</a><br><br>
+                                <a><p>Email:</p>${user.subjects}</a>
                             </div>  
                         </div> 
                     </div> 
                 </div> 
                     `;
-                })
-                .join(""); 
-            document.querySelector('#teacherlist').insertAdjacentHTML('afterbegin', html);
-      } 
-    });
+                    })
+                    .join("");
+
+                document.querySelector('#teacherlist').insertAdjacentHTML('afterbegin', html);
+            }
+        });
 } 
